700a1b00ed9a893cb2db31b47722d3ff03f62d96
kicucokko <91193214+kicucokko@users.noreply.github.com>
2022-01-29 18:05:21 +0900

    マイページの実装
    ・ログイン時のルーティング/View実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
-  
+
+  def after_sign_up_path_for(resource)
+    case resource
+    when User
+      user_path(current_user.id)
+    end
+  end
+  def after_sign_in_path_for(resource)
+    case resource
+    when User
+      user_path(current_user.id) 
+    end
+  end
 end
