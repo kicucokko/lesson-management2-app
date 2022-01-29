882bd330307b1525919cb421f3d7a4bc64804866
kicucokko <91193214+kicucokko@users.noreply.github.com>
2022-01-29 17:33:10 +0900

    新規登録とログインページの実装
    ・devise：userコントローラーの実装
    ・sing_up/sign_in画面の実装
    ・Home画面の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :roles, through: :user_roles
+  
+  with_options presence: true do
+    validates :nickname, presence: true
+    validates :name
+    validates :name_kana
+    validates :birth_date
+    validates :prefecture_id
+    validates :city_block
+  end
+  extend ActiveHash::Associations::ActiveRecordExtensions 
+  belongs_to :prefecture
 end
