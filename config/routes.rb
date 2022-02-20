00d85d54117960f0011e8961421426e232424362
kicucokko <91193214+kicucokko@users.noreply.github.com>
2022-02-20 15:05:52 +0900

    微調整
    ・root_to　修正(自動でsign_inにアクセスしてしまう)
    ・customerマイページ修正(レッスン日が反映されていない）

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  root  'homes#index'
+
   devise_for :users, controllers: {
     sessions:      'users/sessions',
     passwords:     'users/passwords',
     registrations: 'users/registrations' 
   }
-
-  root to: 'homes#index'
   resources :users, only:[:show,:edit,:destroy]
   resources :lesson_rooms, only:[:new,:create,:show,:edit,:update,:destroy] do
     resources :reviews,only:[:new,:create,:show,:edit,:update,:destroy]
