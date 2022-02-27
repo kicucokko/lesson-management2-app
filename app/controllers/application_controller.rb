ca8d99fb3c4ad572e6dbf24e49102f83a88aeaf8
kicucokko <91193214+kicucokko@users.noreply.github.com>
2022-02-27 23:15:55 +0900

    一部修正と認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-
+  before_action :basic_auth
   def after_sign_up_path_for(resource)
     case resource
     when User
@@ -14,5 +14,13 @@ class ApplicationController < ActionController::Base
   end
   rescue_from CanCan::AccessDenied do |_exception|
     redirect_to user_url(current_user), alert: '操作権限がありません。'
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
